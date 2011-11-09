a80aba97ee4f1bd50fecfcc374444974f4f28503
Giuseppe Montesanto <montesanto.giuseppe@live.it>
2011-11-09 21:11:25 +0100

    Unsigned changes.

--- signature_maker/images/index.php
+++ signature_maker/images/index.php
@@ -18,24 +18,25 @@
     <body>
         <center>
             <h3>Seleziona un'immagine</h3>
-            <table cellSpacing="0" cellPadding="4" border="1" style="border-collapse: collapse" bordercolor="111111">
+            <table cellSpacing="0" cellPadding="4" border="1" style="border-collapse: collapse" borderColor="111111">
                 <?php
                     include("/../config.php");
                     foreach($backgrounds as $i => $value)
                     {
                         if($i) print "                "; //Gli indici sono numeri, quindi non è necessario usare un altro contatore.
-                        print "<tr>\n";
-                        print "                    <td>\n";
-                        print "                        <center><a href=\"#ancora_testo\" onClick=\"select('$value');\"><img width=\"350\" src=\"bg/$value.png\" onContextMenu=\"return false;\"></a></center>\n";
-                        print "                    </td>\n";
-                        print "                </tr>\n";
+                        print "<tr>\r\n";
+                        print "                    <td>\r\n";
+                        print "                        <center><a href=\"#ancora_testo\" onClick=\"select('$value');\"><img width=\"350\" src=\"bg/$value.png\" onContextMenu=\"return false;\"></a></center>\r\n";
+                        print "                    </td>\r\n";
+                        print "                </tr>\r\n";
                     }
                 ?>
                 <tr>
                     <td>
                         <center>
                             <a name="ancora_testo">
-                            <input type="text" name="testo_immagine" size="15" style="text-align: center" onClick="selezionaTesto();"><br>(usa CTRL+C per copiare l'immagine)
+                            <input type="text" name="testo_immagine" size="15" style="text-align: center" onClick="selezionaTesto();"><br>
+                            (usa CTRL+C per copiare l'immagine)
                         </center>
                     </td>
                 </tr>
