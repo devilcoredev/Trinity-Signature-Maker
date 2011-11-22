b69368d62c065538cb99268b13af9cbb252c1146
Giuseppe Montesanto <montesanto.giuseppe@live.it>
2011-11-22 12:01:22 +0100

    Change javascript form validation in 'index.php'.

--- signature_maker/index.php
+++ signature_maker/index.php
@@ -69,22 +69,29 @@
 
                 //Aggiungo nuovi campi alla queryString solo se sono stati definiti.
 
+                //Effettua il cambio solo se è stato inserito il nome del pg.
+                var nome_pg = document.getElementsByName("nome_pg")[0].value;
+                if(nome_pg=='')
+                {
+                    alert("<?php print $error_pg; ?>");
+                    return false;
+                }
+
                 var server = document.getElementsByName("server")[0].value;
-                if(server!='' && server!="undefined")
+                if(server!='')
                 {
                     count++;
                     indirizzo += "?server=" + server;
                 }
 
-                var nome_pg = document.getElementsByName("nome_pg")[0].value;
-                if(nome_pg!='' && nome_pg!="undefined")
+                if(nome_pg!='')
                 {
                     indirizzo += (count++ ? '&' : '?');
                     indirizzo += "nome_pg=" + nome_pg;
                 }
 
                 var sfondo = document.getElementsByName("sfondo")[0].value;
-                if(sfondo!='' && sfondo!="undefined")
+                if(sfondo!='')
                 {
                     indirizzo += (count++ ? '&' : '?');
                     indirizzo += "sfondo=" + sfondo;
@@ -99,14 +106,14 @@
                     }
 
                 var filter = document.getElementsByName("filter")[0].value;
-                if(filter!='' && filter!="undefined")
+                if(filter!='')
                 {
                     indirizzo += (count++ ? '&' : '?');
                     indirizzo += "filter=" + filter;
                 }
 
                 var url_image = encodeURIComponent(document.getElementsByName("url_image")[0].value);
-                if(url_image!='' && url_image!="undefined")
+                if(url_image!='')
                 {
                     indirizzo += (count++ ? '&' : '?');
                     indirizzo += "url_image=" + url_image;
@@ -121,14 +128,14 @@
                     }
 
                 var colore_testo = document.getElementsByName("colore_testo")[0].value;
-                if(colore_testo!='' && colore_testo!="undefined")
+                if(colore_testo!='')
                 {
                     indirizzo += (count++ ? '&' : '?');
                     indirizzo += "colore_testo=" + colore_testo;
                 }
 
                 var text_font = document.getElementsByName("text_font")[0].value;
-                if(text_font!='' && text_font!="undefined")
+                if(text_font!='')
                 {
                     indirizzo += (count++ ? '&' : '?');
                     indirizzo += "text_font=" + text_font;
@@ -139,14 +146,14 @@
                     if($image_resize_enabled)
                     {
                         print "var x = document.getElementsByName('x')[0].value;\r\n";
-                        print "                if(x!='' && x!=\"undefined\")\r\n";
+                        print "                if(x!='')\r\n";
                         print "                {\r\n";
                         print "                    indirizzo += (count++ ? '&' : '?');\r\n";
                         print "                    indirizzo += \"x=\" + x;\r\n";
                         print "                }\r\n\r\n";
 
                         print "                var y = document.getElementsByName('y')[0].value;\r\n";
-                        print "                if(y!='' && y!=\"undefined\")\r\n";
+                        print "                if(y!='')\r\n";
                         print "                {\r\n";
                         print "                    indirizzo += (count++ ? '&' : '?');\r\n";
                         print "                    indirizzo += \"y=\" + y;\r\n";
@@ -158,7 +165,7 @@
                     if(document.getElementsByName("enable_custom_stat"+i)[0].checked == true)
                     {
                         var custom_stat = encodeURIComponent(document.getElementsByName("custom_stat"+i)[0].value);
-                        if(custom_stat!='' && custom_stat!="undefined")
+                        if(custom_stat!='')
                         {
                             indirizzo += (count++ ? '&' : '?');
                             indirizzo += "custom_stat" + i + '=' + custom_stat;
@@ -167,7 +174,7 @@
                     else
                     {
                         var stat = document.getElementsByName("stat"+i)[0].value;
-                        if(stat!='' && stat!="undefined" && stat!='-')
+                        if(stat!='' && stat!='-')
                         {
                             indirizzo += (count++ ? '&' : '?');
                             indirizzo += "stat" + i + '=' + stat;
@@ -183,77 +190,73 @@
                 if(indirizzo_path[indirizzo_path.length - 1] != '/')
                     indirizzo_path += '/';
 
-                //Effettua il cambio solo se è stato inserito il nome del pg.
-                if(nome_pg!='' && nome_pg!="undefined")
+                var absolute_link = indirizzo_path + indirizzo;
+
+                var direct_link         = document.getElementsByName("direct_link")[0];
+                var html_link           = document.getElementsByName("html_link")[0];
+                var html_armory_link    = document.getElementsByName("html_armory_link")[0];
+                var bbcode_link         = document.getElementsByName("bbcode_link")[0];
+                var bbcode_armory_link  = document.getElementsByName("bbcode_armory_link")[0];
+
+                //Trovo la dimensione del file d'errore (dato che è png sarà molto maggiore delle dimensioni delle firme).
+                var req = new XMLHttpRequest();
+                req.open("GET", absolute_link, false);
+                req.send(null);
+                var headers = req.getResponseHeader("Content-Length");
+
+                //Se la dimensione combacia con quella dell'immagine d'errore allora svuoto i campi, altrimenti la visualizzo normalmente.
+                if(headers != <?php print $dim_dati_errati; ?>)
                 {
-                    var absolute_link = indirizzo_path + indirizzo;
+                    var armory_template_link = "<?php print $armory_template_link; ?>";
 
-                    var direct_link         = document.getElementsByName("direct_link")[0];
-                    var html_link           = document.getElementsByName("html_link")[0];
-                    var html_armory_link    = document.getElementsByName("html_armory_link")[0];
-                    var bbcode_link         = document.getElementsByName("bbcode_link")[0];
-                    var bbcode_armory_link  = document.getElementsByName("bbcode_armory_link")[0];
+                    var server_keys          = new Array();
+                    var server_armory_names  = new Array();
 
-                    //Trovo la dimensione del file d'errore (dato che è png sarà molto maggiore delle dimensioni delle firme).
-                    var req = new XMLHttpRequest();
-                    req.open("GET", absolute_link, false);
-                    req.send(null);
-                    var headers = req.getResponseHeader("Content-Length");
-
-                    //Se la dimensione combacia con quella dell'immagine d'errore allora svuoto i campi, altrimenti la visualizzo normalmente.
-                    if(headers != <?php print $dim_dati_errati; ?>)
-                    {
-                        var armory_template_link = "<?php print $armory_template_link; ?>";
-
-                        var server_keys          = new Array();
-                        var server_armory_names  = new Array();
-
-                        <?php
-                            $index = 0;
-                            foreach($armory_name as $i => $value)
+                    <?php
+                        $index = 0;
+                        foreach($armory_name as $i => $value)
+                        {
+                            if($value != '')
                             {
-                                if($value != '')
-                                {
-                                    if($index) print "                        ";
-                                    print "server_keys[$index]          = \"$i\";\r\n";
-                                    print "                        ";
-                                    print "server_armory_names[$index]  = \"$value\";\r\n";
-                                    $index++;
-                                }
+                                if($index) print "                    ";
+                                print "server_keys[$index]          = \"$i\";\r\n";
+                                print "                    ";
+                                print "server_armory_names[$index]  = \"$value\";\r\n";
+                                $index++;
                             }
-                        ?>
+                        }
+                    ?>
 
-                        var armory_server_name = '';
+                    var armory_server_name = '';
 
-                        for(var i=0; i<server_keys.length; ++i)
-                            if(server_keys[i] == server && armory_server_name=='')
-                                armory_server_name = server_armory_names[i];
+                    for(var i=0; i<server_keys.length; ++i)
+                        if(server_keys[i] == server && armory_server_name=='')
+                            armory_server_name = server_armory_names[i];
 
                         var armory_link = armory_template_link.replace("%s", armory_server_name).replace("%p", massimizzaTesto(nome_pg));
 
-                        //Inserisco i link nelle caselle di testo.
-                        direct_link.value         = absolute_link;
-                        html_link.value           = "<img src=\"" + absolute_link + "\">";
-                        html_armory_link.value    = "<a href=\"" + armory_link + "\"><img src=\"" + absolute_link + "\"></a>";
-                        bbcode_link.value         = "[img]" + absolute_link + "[/img]";
-                        bbcode_armory_link.value  = "[url=" + armory_link + "][img]" + absolute_link + "[/img][/url]";
-                    }
-                    else
-                    {
-                        direct_link.value         = '';
-                        html_link.value           = '';
-                        html_armory_link.value    = '';
-                        bbcode_link.value         = '';
-                        bbcode_armory_link.value  = '';
-                    }
+                    //Inserisco i link nelle caselle di testo.
+                    direct_link.value         = absolute_link;
+                    html_link.value           = "<img src=\"" + absolute_link + "\">";
+                    html_armory_link.value    = "<a href=\"" + armory_link + "\"><img src=\"" + absolute_link + "\"></a>";
+                    bbcode_link.value         = "[img]" + absolute_link + "[/img]";
+                    bbcode_armory_link.value  = "[url=" + armory_link + "][img]" + absolute_link + "[/img][/url]";
+                }
+                else
+                {
+                    direct_link.value         = '';
+                    html_link.value           = '';
+                    html_armory_link.value    = '';
+                    bbcode_link.value         = '';
+                    bbcode_armory_link.value  = '';
+                }
 
-                    document.getElementById("firma").src = absolute_link; //Modifico il path dell'immagine.
-                    document.getElementById("output").style.display = "block"; //Visualizzo l'output.
+                document.getElementById("firma").src = absolute_link; //Modifico il path dell'immagine.
+                document.getElementById("output").style.display = "block"; //Visualizzo l'output.
 
-                    //Effettuo lo switch delle immagini solo se la firma non è stata già caricata.
-                    if(!document.getElementById("firma").complete)
-                        switchImage(false);
-                }else alert("<?php print $error_pg; ?>");
+                //Effettuo lo switch delle immagini solo se la firma non è stata già caricata.
+                if(!document.getElementById("firma").complete)
+                    switchImage(false);
 
                 return true;
             }
